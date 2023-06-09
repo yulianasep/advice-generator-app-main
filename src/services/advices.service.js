2359ebcbebc59f12dd05acccdb73deff34e664e3
yulianasep <yuliana.sepulveda@alternova.com>
2023-06-09 17:09:51 -0500

    feat: fetching advice data

--- src/services/advices.service.js
+++ src/services/advices.service.js
@@ -2,9 +2,10 @@ const API_URL = "https://api.adviceslip.com/advice";
 
 export async function fetchAdvices() {
   const response = await fetch(API_URL);
+  const data = await response.json();
   if (!response.ok) {
     throw new Error("Something went wrong with the fetch");
   }
-  console.log(response);
-  return response.json();
+
+  return data;
 }
