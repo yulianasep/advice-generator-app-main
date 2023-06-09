9b33d77ef3ad329eefe1b9f3b09daf5e68dc06d3
yulianasep <yuliana.sepulveda@alternova.com>
2023-06-09 14:54:09 -0500

    feat: initial structure

---
+++ src/scripts/app.js
@@ -0,0 +1,8 @@
+"use strict";
+
+import { fetchAdvices } from "../services/advices.service.js";
+
+const $adviceTitle = document.querySelector(".advice-title");
+const $adviceText = document.querySelector(".advice-text");
+console.log($adviceTitle);
+console.log($adviceText);
---
+++ src/services/advices.service.js
@@ -0,0 +1,9 @@
+const API_URL = "https://api.adviceslip.com/advice";
+
+export async function fetchAdvices() {
+  const response = await fetch(API_URL);
+  if (!response.ok) {
+    throw new Error("Something went wrong with the fetch");
+  }
+  return response.json();
+}
